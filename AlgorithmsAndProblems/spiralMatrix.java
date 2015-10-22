28888fad2fe8b5798456b3d2c284442780bb14d7
Jason <jasonmcromer@gmail.com>
2015-10-21 19:48:39 -0700

    Updated comment

--- AlgorithmsAndProblems/spiralMatrix.java
+++ AlgorithmsAndProblems/spiralMatrix.java
@@ -1,10 +1,9 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+/*Parse through the matrix by each row and column, then decrement them as you
+  spiral around. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
